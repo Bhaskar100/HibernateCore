bbe67e3cf5ecc2f874af62665cad06dc3243937e
kk <kk@DESKTOP-FVU0OLT.mshome.net>
2018-12-29 13:27:44 +0530

    first code changes

---
+++ HibernateCore/src/main/java/com/hibernate/ClientTest.java
@@ -0,0 +1,25 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ClientTest {
+
+	public static void main(String args[]) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction t = session.beginTransaction();
+
+		Employee emp = new Employee(11, "ABCD");
+
+		session.persist(emp);
+		t.commit();
+		session.close();
+		System.out.println("successfully table created");
+	}
+}
